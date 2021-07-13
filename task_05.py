ae3a22dc3246d14b1a347e4cedad1fa158f1a2aa
Ayanda Latsha <ayanda.latsha@gmail.com>
2021-07-13 10:35:44 +0200

    added task_05 without builtin modules

--- task_05.py
+++ task_05.py
@@ -1,10 +1,8 @@
-from math import sqrt
-
 def triangle_area(side_a, side_b, side_c):
     semiparameter = 1 / 2 * (side_a + side_b + side_c)
-    area = sqrt(semiparameter * (semiparameter - side_a) *
-            (semiparameter - side_b) * (semiparameter - side_c))
+    area = (semiparameter * (semiparameter - side_a) * (semiparameter - side_b)
+            * (semiparameter - side_c)) ** 0.5
     return area
 
 if __name__ == "__main__":
-    print(triangle_area(6, 6, 6))
+    print(triangle_area(4, 5, 3))
